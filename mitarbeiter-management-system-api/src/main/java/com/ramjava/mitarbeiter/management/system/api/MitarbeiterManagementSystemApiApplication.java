7c6e47a205b28a80dceaecee5b3ea6275a57a789
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2023-12-23 02:03:48 +0800

    Erstes commit des mitarbeiterverwaltungssystems

---
+++ mitarbeiter-management-system-api/src/main/java/com/ramjava/mitarbeiter/management/system/api/MitarbeiterManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.mitarbeiter.management.system.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitarbeiterManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitarbeiterManagementSystemApiApplication.class, args);
+	}
+
+}
